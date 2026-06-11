77846583df08e7ecef53a2cdc5cb5f58223b70ad
Tayyab Atiq <ranatayyab941@gmail.com>
2026-06-11 23:52:45 +0500

    fix: import TargetUsers in App.jsx to resolve ReferenceError

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import ProblemSolution from './components/ProblemSolution';
 import Features from './components/Features';
+import TargetUsers from './components/TargetUsers';
 import HowItWorks from './components/HowItWorks';
 import CTASection from './components/CTASection';
 import About from './components/About';
